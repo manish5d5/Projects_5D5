b5b5563e2b526eeeeeb1529edef16127904a3ee2
manish5d5 <manish14536@gmail.com>
2024-09-14 13:12:18 +0530

    RideShare
    Please go through out Abstract
    
    Co-Authored-By: AbhiramDaita631 <181552457+abhiramdaita631@users.noreply.github.com>
    Co-Authored-By: rakshitharagi9 <145577173+rakshitharagi9@users.noreply.github.com>

---
+++ RIDESHARE/authentication/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(PassengerTable)
+
+admin.site.register(PublisherTable)
+
+admin.site.register(ClientTable)
+
+
+# Register your models here.
